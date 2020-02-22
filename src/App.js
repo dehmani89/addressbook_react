b1aee3519fc61aaf5a006d3464e2c43e3d0e8565
Amine Dehmani <amine.dehmani@gmail.com>
2020-02-21 22:19:53 -0500

    Adding latest changes to the react app to read data from the web service

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import "./App.css"
+import "./components/card.css"
 
 class App extends Component {
 
@@ -21,8 +22,80 @@ class App extends Component {
 
         return (
             <div className="container">
-<br/>
 
+
+                <section id="team" class="pb-5">
+                        <h5 class="section-title h1">Address List</h5>
+                        <div class="row">
+
+
+                            {this.state.addresses.map((address) => (
+
+                                    <div className="col-xs-12 col-sm-6 col-md-4">
+                                        <div className="image-flip" onTouchStart="this.classList.toggle('hover');">
+                                            <div className="mainflip">
+                                                <div className="frontside">
+                                                    <div className="card">
+                                                        <div className="card-body text-center">
+                                                            <p><img className=" img-fluid"
+                                                                    src="images/address-icon-png-256_256.png"
+                                                                    alt="card image"></img></p>
+                                                            <h4 className="card-title">{address.username} Address</h4>
+                                                            <p className="card-text">This is basic card with image on
+                                                                top, title, description and button.</p>
+
+                                                            <a href="#" className="btn btn-primary btn-sm"><i
+                                                                className="fa fa-plus"></i></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="backside">
+                                                    <div className="card">
+                                                        <div className="card-body text-center mt-4">
+                                                            <h4 className="card-title">Edit User Address</h4>
+                                                            <p className="card-text">Please click on the edit button to update a user's Address</p>
+                                                            <ul className="list-inline">
+                                                                <li className="list-inline-item">
+                                                                    <a className="social-icon text-xs-center"
+                                                                       target="_blank" href="#">
+                                                                        <i className="fa fa-edit fa-2x"></i>
+                                                                    </a>
+                                                                </li>
+
+                                                                <li className="list-inline-item">
+                                                                    <a className="social-icon text-xs-center"
+                                                                       target="_blank" href="#">
+                                                                        <i className="fa fa-trash fa-2x"></i>
+                                                                    </a>
+                                                                </li>
+
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                            ))}
+
+
+
+
+
+
+
+
+
+
+
+                        </div>
+                </section>
+
+
+<br/>
+{/*
                <div className="card border-dark mb-3">
                     <h1 className="text-center"><i className="fa fa-address-card fa-lg" aria-hidden="true"></i>
                          Address List</h1>
@@ -45,6 +118,11 @@ class App extends Component {
                         </div>
                     ))}
                 </div>
+
+ */}
+
+
+
             </div>
         );
     }
